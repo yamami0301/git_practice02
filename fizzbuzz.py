1981197b2dc908ac08223e2fb0af6de122b39f5d
yamami0301 <allstar.naikaku6@gmail.com>
2020-07-02 09:38:29 +0900

    3の倍数のときのfizzbuzzに対応

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,6 +1,17 @@
 def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return 'FizzBuzz'
+
+    if number % 5 == 0:
+        return 'Buzz'
+
     if number % 3 == 0:
         return 'Fizz'
 
 
+assert fizzbuzz_convert(15) == 'FizzBuzz'
+assert fizzbuzz_convert(30) == 'FizzBuzz'
+assert fizzbuzz_convert(5) == 'Buzz'
+assert fizzbuzz_convert(10) == 'Buzz'
 assert fizzbuzz_convert(3) == 'Fizz'
+assert fizzbuzz_convert(6) == 'Fizz'
